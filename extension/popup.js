22ea4a46d90fb1baeb8ade488f0bb42f9db0cb09
Wadiya Khaba Admin <asoffical50@gmail.com>
2026-03-04 21:44:08 +0500

    Add Hot Dog product and setup automations

---
+++ extension/popup.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-wa').addEventListener('click', () => {
+    window.open('https://wa.me/923701431247', '_blank');
+});
+
+document.getElementById('btn-web').addEventListener('click', () => {
+    // GitHub repository URLs use hyphens instead of spaces
+    window.open('https://asoffical50.github.io/Wadhiya-Khaba-Home-Food/', '_blank');
+});
